b93f09489595ec07f569975c3aff9eb0800d2d46
frogsandcode <mcoltmanns@gmail.com>
2019-03-14 07:36:25 -0400

    Add files via upload

---
+++ Kahoot_Quizizz_Smash/Kahoot_Quizizz_Smash_win.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import threading
+import platform
+
+
+class KahootBot:
+    def __init__(self, pin, name):
+        print("\nConnecting to game at " + pin + " as " + name)
+
+        if platform.system() == 'Windows':
+            self.driver = webdriver.Chrome(executable_path='webDrivers\chromedriverwin.exe')
+        elif platform.system() == 'Darwin':
+            self.driver = webdriver.Chrome(executable_path='webDrivers/chromedrivermac')
+
+        self.driver.get('http://www.kahoot.it')
+
+        self.driver.implicitly_wait(2)
+
+        self.pin = pin
+
+        # now find the key input box and pass it the key
+        self.driver.find_element_by_id('inputSession').send_keys(pin)
+        # now find the submit button and click it
+        self.driver.find_element_by_xpath("//button[@type='submit']").click()
+
+        # now wait for javascript
+        self.driver.implicitly_wait(2)
+
+        # now find the name input box and pass it the name
+        self.nameIn = self.driver.find_element_by_id("username")
+        self.nameIn.send_keys(name)
+        # now hit enter to submit the username
+        self.nameIn.send_keys(Keys.RETURN)
+
+        while True:
+            pass
+
+
+def createBot(pin, name):
+    bot = KahootBot(pin, name)
+
+
+choice = int(input("Welcome to the Kahoot/Quizizz smasher.\nTo smash a Kahoot, press 1. To smash a Quizizz, press 2.\n"
+                   "Chrome is required\nWARNING: Smashing Kahoots and Quizizzs takes a lot of chrome windows! Try to"
+                   " free up as much ram as possible before smashing something"))
+
+if choice == 1:
+    pin = str(input("You have chosen to smash a Kahoot.\nInput the pin: "))
+    botNum = int(input("How many bots? "))
+    baseName = str(input("What should they be named? "))
+
+    for x in range(botNum):
+        name = str(baseName + str(x))
+        botThread = threading.Thread(target=createBot, args=(pin, name))
+        botThread.start()
